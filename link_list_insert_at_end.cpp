a759ca40acecc36c850ffc21f43a3cf01f0ca14a
adilhusain01 <husainadil202@gmail.com>
2023-09-01 18:58:30 +0530

    ...

--- link_list_insert_at_end.cpp
+++ link_list_insert_at_end.cpp
@@ -2,63 +2,59 @@
 
 using namespace std;
 
-class Node{
-    public:
-
+class Node {
+public:
     int data;
     Node* next;
 
-    //Node constructor
     Node(int val) : data(val), next(nullptr) {}
 };
 
-
-class LinkedList{
-    private:
+class LinkedList {
+private:
     Node* head;
 
-    public:
-
-    //List Constructor
+public:
     LinkedList() : head(nullptr) {}
 
-    void insertAtEnd(int val){
-        Node* newnode=new Node(val);
-        if(head==nullptr){
-            head=newnode;
+    void insertAtEnd(int val) {
+        Node* newnode = new Node(val);
+        if (head == nullptr) {
+            head = newnode;
             return;
         }
-        Node* current=head;
-        while(current->next!=nullptr){
-            current=current->next;
+        Node* current = head;
+        while (current->next != nullptr) {
+            current = current->next;
         }
-        current->next=newnode;
+        current->next = newnode;
     }
 
-    void print(){
-        Node* current=head;
-        while(current!=nullptr){
-            cout<<current->data<<" -> ";
-            current=current->next;
+    void print() {
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->data << " -> ";
+            current = current->next;
         }
-        cout<<nullptr;
+        cout << "nullptr" << endl;
     }
 };
 
-int main(){
+int main() {
     int size;
-    cout<<"Enter the amount of data inputs : ";
-    cin>>size;
+    cout << "Enter the amount of data inputs: ";
+    cin >> size;
 
     LinkedList mylist;
-    
-    for(int i=0; i<size; i++){
+
+    for (int i = 0; i < size; i++) {
         int temp;
-        cout<<"Enter data item "<<i+1<<" to insert at beginning : ";
-        cin>>temp;
+        cout << "Enter data item " << i + 1 << " to insert at end: ";
+        cin >> temp;
         mylist.insertAtEnd(temp);
     }
-    
+
+    cout << "Linked List: ";
     mylist.print();
 
     return 0;
