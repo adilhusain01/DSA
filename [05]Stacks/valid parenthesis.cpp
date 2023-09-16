0849cb75bfaacf1e9c1c94bda8494f2911949b2b
adilhusain01 <husainadil202@gmail.com>
2023-09-16 14:34:16 +0530

    ...

---
+++ [05]Stacks/valid parenthesis.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool validParenthesis(string str){
+    stack <char> s;
+    for(int i=0; i<str.size(); i++){
+        char ch=str[i];
+
+        if(ch=='{' || ch=='(' || ch=='[') s.push(ch);
+        else{
+            if(!s.empty()) {
+                char top=s.top();
+                if((ch=='}' && top=='{') || (ch==']' && top=='[') || (ch==')' && top=='(') ) s.pop();
+                else return false;
+            } else {
+                 return false;
+            }
+        }
+    }
+    if (s.empty()) return true;
+    else return false;
+};
+
+//{[()]}
+//{([][])()}
+//only {[( are inserted into the stack and then top of tthe stack is compared with the current ch eg: } or ) or ] 
+//and if it matches it gets popped and if not then its not valid
+
+int main(){
+    string str;
+    cout<<"Enter the parenthesis sequence : ";
+    getline(cin, str);
+
+    if (validParenthesis(str)) cout<<"The parenthesis sequence : "<<str<<" is valid"<<endl;    
+    else cout<<"The parenthesis sequence : "<<str<<" is invalid"<<endl; 
+
+    return 0;
+}
