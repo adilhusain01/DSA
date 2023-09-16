69e1004aac4b8234cf1d36000ad494b662fff79e
adilhusain01 <husainadil202@gmail.com>
2023-09-16 12:02:24 +0530

    ...

--- [05]Stacks/remove_middle_element.cpp
+++ [05]Stacks/remove_middle_element.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Method 1
+/*
 void removeMid(stack <int> & s){
     int mid=s.size()/2;
     int temp[mid];
@@ -24,6 +26,42 @@ int main(){
     s.push(1);
 
     removeMid(s);
+
+    while(!s.empty()) {
+        cout<<s.top()<<endl;
+        s.pop();
+    }
+
+    return 0;
+}
+*/
+
+//Method2
+//Better than above, uses recursive call
+
+void removeMid(stack <int> & s, int count, int size){
+    if (count==size/2){
+        s.pop();
+        return;
+    }
+
+    int temp=s.top();
+    s.pop();
+    removeMid(s, ++count, size);
+    s.push(temp);
+}
+
+int main(){
+    stack <int> s;
+    s.push(5);
+    s.push(4);
+    s.push(3);
+    s.push(2);
+    s.push(1);
+
+    int count=0;
+
+    removeMid(s, count, s.size());
 
     while(!s.empty()) {
         cout<<s.top()<<endl;
