da5a5745015be3d3fe2a855c3ffa9b39d4f11851
adilhusain01 <husainadil202@gmail.com>
2023-09-18 19:39:08 +0530

    ...

--- [05]Stacks/redundant_bracket.cpp
+++ [05]Stacks/redundant_bracket.cpp
@@ -1,6 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//((a+b)) here first bracket is redundant as they are needless
+
+void checkRedundant(string & str){
+    stack <char> s;
+    for(int i=0 ; i<str.size(); i++){
+        char ch=str[i];
+        if (ch=='(' || ch=='*' || ch=='+' || ch=='-' || ch=='/') s.push(ch);
+        else if(ch==')'){
+            char top=s.top();
+            if (top=='*' || top=='-' || top=='+' || top=='/'){
+                s.pop();
+                if(s.top()=='(') s.pop();
+            }
+        }
+    }
+
+    if(s.empty()) cout<<"The brackets were not redundant"<<endl;
+    else cout<<"The brackets were redundant"<<endl;
+}
+
 int main(){
-    
+    string str;
+    cout<<"Enter the input : ";
+    cin>>str;
+
+    checkRedundant(str);
+
+    return 0;
 }
---
+++ [06]Queue/queue_readme.txt
@@ -0,0 +1,8 @@
+//FIFO linear data structure
+//First element -> Front
+//Last element -> Rear
+
+Functions : 
+            front()
+            back()
+            push()
